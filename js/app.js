19a229c552d14f42a00411cd4239ada0633fed8a
Flow-Fly <florian.aube@gmail.com>
2021-07-13 15:53:03 +0200

    Bunch of changes, too long since last commit

--- js/app.js
+++ js/app.js
@@ -4,9 +4,18 @@ import {Ship, BattleShips, Field} from '../modules/classes.js'
 const playerBoard = document.querySelector('.playerBoard')
 const shipsBoard = document.querySelector('.shipsBoard')
 const start = document.getElementById('start')
-const shipsInventory = document.querySelector('.shipsInventory')
+const recap = document.querySelector('.recap')
 const randomBtn = document.getElementById('randomizer')
+const fireBtn = document.getElementById('fire')
+const infos = document.getElementById('computerInfos')
+const compHits = document.getElementById('compHits')
+const compMiss = document.getElementById('compMiss')
+const compShips = document.getElementById('compShips')
+const playHits = document.getElementById('playHits')
+const playMiss = document.getElementById('playMiss')
+const playShips = document.getElementById('playShips')
 let dragged = null
+fireBtn.disabled = true
 
 
 
@@ -26,63 +35,126 @@ const gameState = {
 }
 
 
-
-
-
-//Used to try to snap the boats on the grid
-let shipBoardRect = shipsBoard.getBoundingClientRect()
-shipBoardRect = {
-    x: parseInt(shipBoardRect.x + 16, 10),
-    y: parseInt(shipBoardRect.y + 16, 10),
-    xmax: parseInt(shipBoardRect.right - 16, 10),
-    ymax: parseInt(shipBoardRect.bottom - 16, 10)
-}
-console.log(`x: ${shipBoardRect.x} to ${shipBoardRect.xmax}
-y: ${shipBoardRect.y} to: ${shipBoardRect.ymax}`)
-
-
 gameSetup()
 generateInventory()
-dragNDrop()
+
+/* dragNDrop() */
 
 
 randomlyPlaceShips(playerShips, playerField)
-randomlyPlaceShips(computerShips, computerField)
+//randomlyPlaceShips(computerShips, computerField)
+console.log(computerField.field)
 paintShips()
 
 start.onclick = () => {
+    randomBtn.onclick = () => {}
     mainGame()
 }
 
 
 function mainGame() {
     const turn = gameState.turn % 2 === 0 ? 'computer' : 'player'
-
+    console.log(turn)
+    infos.innerHTML += `<br><p>Turn ${gameState.turn}:</p><h3>It's ${turn}'s turn!</h3><br>`
     if(turn === 'player') {
-        playerBoard.querySelectorAll('.water').forEach(cell => {
-            cell.onclick = () => {
-                cell.classList.toggle('shooting')
-                
-            }
-        }) 
+        pickShots()
+        waitForX(playerShips.army.length, turn, computerField.field)
+        
+    } else {
+        if (computerField.hits.length === 0) {
+            const shots = computerField.randomShot(computerShips.army.length)
+            fire(turn, playerField.field, shots)
+        } else {
+            
+        }
     }
 }
 
-
-randomBtn.onclick = () => {
-    playerField.createField()
-    shipsBoard.querySelectorAll('.ship').forEach(ship => ship.classList.replace('ship', 'empty'))
-    randomlyPlaceShips(playerShips, playerField)
-    paintShips()
+function pickShots() {
+    infos.innerHTML += `<p>Set up to ${playerShips.army.length} shots!</p>`
+    playerBoard.querySelectorAll('.water').forEach(cell => {
+        cell.onclick = () => {
+            if (!cell.classList.contains('locked')) {
+                if (!cell.classList.contains('shooting')) {
+                    cell.classList.remove('water')
+                    cell.classList.add('shooting')
+                }
+            }
+            else if (cell.classList.contains('shooting')) {
+                cell.classList.remove('shooting')
+                cell.classList.add('water')
+            }
+        }
+    })
 }
 
-function generateShips(str) {
-    str.addShip(new Ship('Carrier', 5))
-    str.addShip(new Ship('Battleship', 4))
-    str.addShip(new Ship('Destroyer', 3))
-    str.addShip(new Ship('Submarine', 3))
-    str.addShip(new Ship('PatrolBoat', 2))
+function waitForX(remainingShips, turn, field) {
+    let intervalId = setInterval(() => {
+        const cells = playerBoard.querySelectorAll('.shooting')
+        console.log(`remaining ships: ${remainingShips}, shots: ${cells.length}`)
+        if (cells.length >= remainingShips) {
+            playerBoard.querySelectorAll('.water').forEach(cell => {
+                cell.classList.add('locked')
+            })
+            fireBtn.disabled = false
+            fireBtn.onclick = () => {
+                clearInterval(intervalId)
+                fire(turn, field)
+            }
+        }
+    },50)
 }
+
+function fire(turn, field, shots) {
+    if (turn === 'player') {
+        playerBoard.querySelectorAll('.shooting').forEach(cell => {
+            cell.classList.remove('shooting')
+            cell.classList.add('shot', 'locked')
+        })
+        playerBoard.querySelectorAll('.water.locked').forEach(cell => {
+            cell.classList.remove('locked')
+        })
+        playerBoard.querySelectorAll('.shot').forEach(cell => {
+            if (itsAShip(turn, cell, field)) {
+                cell.classList.add('hit')
+            } else {
+                cell.classList.add('miss')
+            }
+        })
+        
+    } else {
+        shots.forEach(shot => {
+            const x = shot.x
+            const y = shot.y
+            const cell = shipsBoard.querySelector(`[x="${x}"][y="${y}"]`)
+            if (itsAShip(turn, shot, field)) {
+                cell.classList.add('hit')
+            } else {
+                cell.classList.add('miss')
+            }
+        })
+    }
+    endTurn(turn)
+}
+
+function endTurn(turn) {
+    gameState.playerHits = playerBoard.querySelectorAll('.hit').length
+    gameState.computerHits = playerField.totalHits
+    gameState.playerMiss = playerBoard.querySelectorAll('.miss').length
+    gameState.computerMiss = playerField.totalMiss
+    turn === 'player' ? sinkShip(turn, computerShips) : sinkShip(turn, playerShips)
+    compHits.textContent = gameState.computerHits
+    compMiss.textContent = gameState.computerMiss
+    compShips.textContent = computerShips.army.length
+    playHits.textContent = gameState.playerHits
+    playMiss.textContent = gameState.playerMiss
+    playShips.textContent = playerShips.army.length
+    console.log(gameState)
+
+    gameState.turn += 1
+    mainGame()
+}
+
 
 function generateInventory() {
     
@@ -97,20 +169,16 @@ function generateInventory() {
             shipsBoard.append(div)
         }
     }
-    
-    //Add all the ships to the inventory
-    playerShips.army.forEach((ship, index) => {
-        const div = document.createElement('div');
-        div.id = ship.name
-        div.classList.add('invShips')
-        //div.style.gridArea = `${6 - ship.length} / ${index + 1} / ${6} / ${index + 2}`
-        div.style.gridRowEnd = `span ${ship.length}`
-        div.style.backgroundColor = '#FFB703';
-        div.setAttribute('draggable', true)
-        
-        shipsInventory.appendChild(div)
-    })
 }
+
+function generateShips(str) {
+    str.addShip(new Ship('Carrier', 5))
+    str.addShip(new Ship('Battleship', 4))
+    str.addShip(new Ship('Destroyer', 3))
+    str.addShip(new Ship('Submarine', 3))
+    str.addShip(new Ship('PatrolBoat', 2))
+}
+
 
 function gameSetup() {
     let map = playerField.createField(true)
@@ -118,12 +186,133 @@ function gameSetup() {
         playerBoard.appendChild(cell)    
 });
     computerField.createField(false)
+    computerField.randomMoves()
     
     generateShips(playerShips)
     generateShips(computerShips)
+
+
+    randomlyPlaceShips(computerShips, computerField)
+    randomBtn.onclick = () => {
+        playerField.createField()
+        shipsBoard.querySelectorAll('.ship').forEach(ship => ship.classList.replace('ship', 'empty'))
+        randomlyPlaceShips(playerShips, playerField)
+        paintShips()
+    }
 }
 
-function dragNDrop() {
+
+
+function randomlyPlaceShips(army, field) {
+   
+    army['army'].forEach(ship => {
+        //Get a random direction: horizontal (0) vertical (1)
+        const direction = Math.floor(Math.random() * 2)
+        let x = null
+        let y = null
+        let counter = 0
+        //horizontal
+        if (direction === 0) {
+            do {
+                //reset counter to 0
+                counter = 0
+                //Get a x position that will make the boat fit
+                x = Math.floor(Math.random() * (10 - ship.length))
+                y = Math.floor(Math.random() * 10)
+                for (let i = 0; i < ship.length; i++) {
+                    //field[][].ship is set to false as there is no ship.
+                    //If all cells are false (free), end the while loop.
+                    //Performance is quite bad as it's a while loop with random assignements.
+                    //Could end up in an infinite loop with a lot of bad luck.
+                    //To improve if time.
+                    if (!field.field[x + i][y].ship)  {
+                        counter += 1
+                    }
+                }
+            } while (counter !== ship.length)
+            //Now that we know all the cells are free, assign them to true and get the name
+            for (let i = 0; i < ship.length; i++) {
+                field.field[x + i][y].ship = true
+                field.field[x + i][y].name = ship.name
+            }
+        } else {
+            //same logic on y axis.
+            do {
+                counter = 0
+                x = Math.floor(Math.random() * (10))
+                y = Math.floor(Math.random() * (10 - ship.length))
+                for (let i = 0; i < ship.length; i++) {
+
+                    if (!field.field[x][y + i].ship) {
+                        counter += 1
+                    }
+                }
+            } while (counter !== ship.length)
+            for (let i = 0; i < ship.length; i++) {
+                field.field[x][y + i].ship = true
+                field.field[x][y + i].name = ship.name
+            }
+        }
+    })   
+}
+
+function itsAShip(turn, cell, field) {
+    let x = null
+    let y = null
+    //Assign x,y depending of who is playing.
+    if (turn === 'player') {
+        x = cell.getAttribute("x")
+        y = cell.getAttribute("y")
+    } else {
+        x = cell.x
+        y = cell.y
+    }
+    //result to true if there is a ship (set in randomlyPlaceShips())
+    if (field[x][y].ship) {
+        const name = field[x][y].name
+        computerShips.army.forEach(ship => {
+            if (ship['name'] === name) {
+                //decrement health of hitted ship
+                ship.health -= 1
+                infos.innerHTML += `<p>${turn} hit ${ship.name} on cell ${x} - ${y}<p/>`
+                //Incrementing counters depending of who played
+                turn === 'player' ? gameState.playerHits += 1 : gameState.computerHits += 1
+            }
+        })
+        return true
+    } else {
+        turn === 'player' ? gameState.playerMiss += 1 : gameState.computerMiss += 1
+        return false
+    }
+}
+
+function sinkShip(turn, army) {
+    const toSink = []
+    if (turn === 'player') {  
+        for (let i = army.army.length - 1; i >= 0; i--) {
+            /* console.log(computerShips.army)
+            console.log(computerShips.army[i]) */
+            if (army.army[i]['health'] === 0) {
+                infos.innerHTML += `<p>${turn} sank ${army.army[i].name}</p>`
+                toSink.push(army.army.splice(i,1))
+            }
+        }
+    }
+}
+
+function paintShips() {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            const cell = playerField.field[i][j]
+            if (cell.ship) {
+                const paintedShip = shipsBoard.querySelector(`[x="${i}"][y="${j}"]`)
+                paintedShip.classList.replace('empty', 'ship')
+            }
+        }
+    }
+}
+
+/* function dragNDrop() {
     document.querySelectorAll('.invShips').forEach(ship => {
         ship.ondragstart = (e) => {
             dragged = ship;
@@ -142,62 +331,30 @@ function dragNDrop() {
         e.target.appendChild(dragged)
         e.dataTransfer.clearData();
     }
-}
+} 
 
-function randomlyPlaceShips(playerArmy, playerField) {
-   
-    const ships = playerArmy.army
-    ships.forEach(ship => {
-        const direction = Math.floor(Math.random() * 2)
-        let x = null
-        let y = null
-        let counter = 0
-        if (direction === 0) {
-            do {
-                counter = 0
-                x = Math.floor(Math.random() * (10 - ship.length))
-                y = Math.floor(Math.random() * 10)
-                for (let i = 0; i < ship.length; i++) {
-                    if (!playerField.field[x + i][y].ship)  {
-                        counter += 1
-                    }
-                }
-            } while (counter !== ship.length)
-            for (let i = 0; i < ship.length; i++) {
-                playerField.field[x + i][y].ship = true
-            }
-        } else {
-            do {
-                counter = 0
-                x = Math.floor(Math.random() * (10))
-                y = Math.floor(Math.random() * (10 - ship.length))
-                for (let i = 0; i < ship.length; i++) {
-                    
-                  
-                    if (!playerField.field[x][y + i].ship) {
-                        counter += 1
-                    }
-                }
-            } while (counter !== ship.length)
-            for (let i = 0; i < ship.length; i++) {
-                playerField.field[x][y + i].ship = true
-            }
-        }
-    })   
-}
 
-function ItsAShip(cell, playerField) {
 
+//Used to try to snap the boats on the grid
+let shipBoardRect = shipsBoard.getBoundingClientRect()
+shipBoardRect = {
+    x: parseInt(shipBoardRect.x + 16, 10),
+    y: parseInt(shipBoardRect.y + 16, 10),
+    xmax: parseInt(shipBoardRect.right - 16, 10),
+    ymax: parseInt(shipBoardRect.bottom - 16, 10)
 }
-
-function paintShips() {
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
-            const cell = playerField.field[i][j]
-            if (cell.ship) {
-                const paintedShip = shipsBoard.querySelector(`[x="${i}"][y="${j}"]`)
-                paintedShip.classList.replace('empty', 'ship')
-            }
-        }
-    }
-}
+console.log(`x: ${shipBoardRect.x} to ${shipBoardRect.xmax}
+y: ${shipBoardRect.y} to: ${shipBoardRect.ymax}`)
+ */
+    //Add all the ships to the inventory
+    /* playerShips.army.forEach((ship, index) => {
+        const div = document.createElement('div');
+        div.id = ship.name
+        div.classList.add('invShips')
+        //div.style.gridArea = `${6 - ship.length} / ${index + 1} / ${6} / ${index + 2}`
+        div.style.gridRowEnd = `span ${ship.length}`
+        div.style.backgroundColor = '#FFB703';
+        div.setAttribute('draggable', true)
+        
+        shipsInventory.appendChild(div)
+    }) */
--- modules/classes.js
+++ modules/classes.js
@@ -10,6 +10,7 @@ export class Ship {
 export class BattleShips {
   constructor() {
     this.army = [];
+    this.deadShips = []
   }
   addShip(ship) {
     this.army.length < 5
@@ -21,11 +22,14 @@ export class BattleShips {
   }
 }
 
+
+
+
+
+
 export class Field {
   constructor() {
-    this.row = 10
-    this.col = 10
-    this.field = new Set
+    this.field = Array(10).fill().map(() => Array(10).fill())
     this.moveMades = new Set
     this.randomPossibleMoves = new Set
     this.hits = new Set
@@ -34,41 +38,64 @@ export class Field {
   createField(bool) {
     //true bool is passed to create played field, false for computer.
     const playerField = []
-    for(let i = 0; i < this.row; i++) {
-      for (let j = 0; j < this.col; j++) {
+    for(let i = 0; i < this.field.length; i++) {
+      for (let j = 0; j < this.field.length; j++) {
         if (bool) {
           const div = document.createElement('div')
-          div.id = `x:${i}-y:${j}`
+          div.setAttribute('x', i)
+          div.setAttribute('y', j)
+          div.classList.add('water')
           playerField.push(div)
         }
-        const cell = {x: i, y: j}
-        this.field.add(cell)
+        this.field[i][j] = {}
+        this.field[i][j].x = i
+        this.field[i][j].y = j
+        this.field[i][j].ship = false
+        this.field[i][j].name = null
       }
     }
     if (bool) return playerField
   }
-
+ 
   randomMoves() {
-    for (let i = 1; i < 10; i += 2) {
-      for (let j = 1; j < 10; j += 2) {
-        this.randomPossibleMoves.add({x: i, y: j})
+    for (let i = 0; i < 10; i++) {
+      if (i % 2 === 0) {
+        for (let j = 1; j < 10; j += 2) {
+          this.randomPossibleMoves.add({x: i, y: j})
+        }
+      } else {
+        for (let j = 0; j < 10; j += 2) {
+          this.randomPossibleMoves.add({x: i, y: j})
+        }
       }
+      
     }
   }
 
-  randomShot() {
-    let shot = Array.from(this.randomPossibleMoves);
-    const index = Math.floor(Math.random() * shot.length);
-    if (!this.moveMades.has(shot[index])) {
-      shot = shot[index];
-      this.randomPossibleMoves.delete(shot)
-      this.moveMades.add(shot)
-      return shot
+  randomShot(num) {
+    const shots = []
+    for (let i = 0; i < num; i++) {
+      let shot = Array.from(this.randomPossibleMoves);
+      const index = Math.floor(Math.random() * shot.length);
+      if (!this.moveMades.has(shot[index])) {
+        shot = shot[index];
+        this.randomPossibleMoves.delete(shot)
+        this.moveMades.add(shot)
+        shots.push(shot)
+      } else {
+        randomShot(num)
+      }
     }
-    randomShot()
+    return shots
   }
 
   hunt() {
-    
+
+  }
+
+  knowledgeCheck() {
+    if (this.hits.length > 1) {
+      
+    }
   }
 }
